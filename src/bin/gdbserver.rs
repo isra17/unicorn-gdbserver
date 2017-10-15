e34ea991cd0e78a376062e0a29307c130373ebc8
isra17 <isra017@gmail.com>
2017-10-15 13:25:14 -0400

    Refactored code

--- src/bin/gdbserver.rs
+++ src/bin/gdbserver.rs
@@ -6,6 +6,8 @@ use unicorn::{CpuX86, Cpu};
 
 fn main() {
     let emu = CpuX86::new(unicorn::Mode::MODE_32).expect("failed to instantiate emulator");
-    let gdbserver = GDBServer::attach(emu.emu());
-    gdbserver.listen();
+    let gdbserver = GDBServer::new(emu.emu(), "127.0.0.1:9999")
+        .expect("Failed to create GDBServer");
+    let mut gdbsession = gdbserver.accept().expect("Failed to accept a client");
+    gdbsession.handle_commands().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,113 +1,63 @@
 extern crate unicorn;
 
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 
 const HEXCHARS: &'static [u8] = b"0123456789abcdef";
 
-pub struct GDBServer<'a> {
-    uc: &'a unicorn::Unicorn,
+pub struct GDBSession<'a> {
+    client : GDBStream,
+    uc : &'a unicorn::Unicorn,
 }
 
-impl<'a> GDBServer<'a> {
-    pub fn attach(uc: &unicorn::Unicorn) -> GDBServer {
-        GDBServer { uc: uc }
+struct GDBStream {
+    socket: TcpStream,
+}
+
+pub struct GDBServer<'a> {
+    uc: &'a unicorn::Unicorn,
+    listener: TcpListener,
+}
+
+enum GDBPacket {
+    Interrupt,
+    MessageReceived,
+    MessageFailed,
+    Command(Vec<u8>),
+}
+
+impl<'a> GDBSession<'a> {
+    fn new(uc : &'a unicorn::Unicorn, socket : TcpStream) -> GDBSession<'a> {
+        GDBSession {client: GDBStream::from_socket(socket), uc: uc}
     }
 
-    pub fn listen(&self) {
-        println!("GDB server listening on port 9999");
-        let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-
-        match listener.accept() {
-            Ok((socket, addr)) => {
-                println!("new client: {:?}", addr);
-                self.handle_connection(socket);
-            }
-            Err(e) => println!("couldn't get client: {:?}", e),
-        }
-
-        self.uc.emu_stop().expect("Failed to stop engine.");
-    }
-
-    fn handle_connection(&self, mut socket: TcpStream) -> () {
+    pub fn handle_commands(&mut self) -> std::io::Result<()> {
         loop {
-            let packet = self.read_packet(&mut socket);
-            println!("{:?}", String::from_utf8(packet.clone()));
-            self.handle_packet(&mut socket, &packet)
-        }
-    }
+            match self.client.read_packet()? {
+                GDBPacket::Command(packet) => {
+                    let code = packet[0];
+                    let response = match code {
+                        b'?' => b"S05".to_vec(),
+                        b'c' => b"S05".to_vec(),
+                        b'D' => break,
+                        b'g' => self.read_all_regs(),
+                        b'H' => b"OK".to_vec(),
+                        b'm' => self.handle_read_memory(&packet),
+                        b'q' => self.handle_query(String::from_utf8(packet.into()).unwrap()).to_vec(),
+                        b's' => b"S05".to_vec(),
+                        _ => {
+                            println!("Unknown command: {:?}", String::from_utf8(packet));
+                            vec![]
+                        }
+                    };
+                    self.client.write_packet(&response)?;
+                },
 
-    fn read_packet(&self, socket: &mut TcpStream) -> Vec<u8> {
-        for b in socket.bytes() {
-            let b = b.unwrap();
-            if b == '$' as u8 {
-                break;
-            } else if b == 3 {
-                println!("Interrupt requested!");
-            } else if b & 0x80 != 0 {
-                panic!("Unexpected byte: {}", b);
-            } else {
-                println!("Ignoring byte: {}", b as char);
+                _ => (),
             }
         }
 
-        let mut buffer = Vec::<u8>::new();
-        for b in socket.bytes() {
-            let b = b.unwrap();
-            if b == '#' as u8 {
-                break;
-            } else if b & 0x80 != 0 {
-                panic!("Unexpected byte: {}", b);
-            } else {
-                buffer.push(b);
-            }
-        }
-
-        let mut checksum = [0; 2];
-        socket.read_exact(&mut checksum).expect("Failed to read checksum");
-
-        socket.write(&['+' as u8]).expect("Failed to write ack");
-
-        buffer
-    }
-
-    fn write_packet(&self, socket: &mut TcpStream, packet: &[u8]) {
-        let mut buffer = Vec::<u8>::with_capacity(packet.len());
-        let mut checksum: u8 = 0;
-        buffer.push('$' as u8);
-        for b in packet {
-            if *b == '$' as u8 || *b == '#' as u8 || *b == '*' as u8 || *b == '}' as u8 {
-                buffer.push('}' as u8);
-                buffer.push(*b ^ 0x20);
-                checksum = checksum.wrapping_add(b'}');
-                checksum = checksum.wrapping_add(*b ^ 0x20);
-            } else {
-                buffer.push(*b);
-                checksum = checksum.wrapping_add(*b);
-            }
-        }
-        buffer.push(b'#');
-        buffer.push(HEXCHARS[checksum as usize >> 4]);
-        buffer.push(HEXCHARS[checksum as usize & 0xf]);
-        socket.write_all(&buffer).expect("Failed to send response");
-        socket.flush().expect("Failed to flush socket");
-    }
-
-    fn handle_packet(&self, socket: &mut TcpStream, packet: &[u8]) {
-        let cmd = packet[0];
-        let response: Vec<u8> = match cmd {
-            b'?' => b"S05".to_vec(),
-            b'c' => b"S05".to_vec(),
-            b'g' => self.read_all_regs(),
-            b'H' => b"OK".to_vec(),
-            b'm' => self.handle_read_memory(packet),
-            b'q' => self.handle_query(String::from_utf8(packet.into()).unwrap()).to_vec(),
-            b's' => b"S05".to_vec(),
-            _ => b"".to_vec(),
-        };
-
-        println!("Response: {:?}", String::from_utf8(response.clone()));
-        self.write_packet(socket, &response);
+        Ok(())
     }
 
     fn handle_query(&self, packet: String) -> &[u8] {
@@ -140,5 +90,88 @@ impl<'a> GDBServer<'a> {
         let _address = u64::from_str_radix(split.next().unwrap(), 16).unwrap();
         let length = u64::from_str_radix(split.next().unwrap(), 16).unwrap();
         return std::iter::repeat(b'0').take(length as usize * 2).collect();
+    }
+}
+
+impl GDBStream {
+    fn from_socket(socket : TcpStream) -> GDBStream {
+        return GDBStream { socket }
+    }
+
+    fn read_packet(&mut self) -> std::io::Result<GDBPacket> {
+        let socket = &mut self.socket;
+        for b in socket.bytes() {
+            let b = b?;
+            if b == b'$' {
+                // Begin command marker.
+                break;
+            } else if b == 3 {
+                // Interrupt (ctrl+c).
+                return Ok(GDBPacket::Interrupt);
+            } else if b == b'+' {
+                return Ok(GDBPacket::MessageReceived);
+            } else if b == b'-' {
+                return Ok(GDBPacket::MessageFailed);
+            } else {
+                panic!("Unexpected byte: {}", b);
+            }
+        }
+
+        let mut buffer = Vec::<u8>::new();
+        for b in socket.bytes() {
+            let b = b?;
+            if b == '#' as u8 {
+                break;
+            } else if b & 0x80 != 0 {
+                panic!("Unexpected byte: {}", b);
+            } else {
+                buffer.push(b);
+            }
+        }
+
+        // Ignore checksum for now.
+        let _checksum = socket.read_exact(&mut [0;2])?;
+        socket.write(b"+")?;
+
+        Ok(GDBPacket::Command(buffer))
+    }
+
+    fn write_packet(&mut self, packet: &[u8]) -> std::io::Result<()>{
+        let socket = &mut self.socket;
+        let mut buffer = Vec::<u8>::with_capacity(packet.len());
+        let mut checksum: u8 = 0;
+        buffer.push('$' as u8);
+        for b in packet {
+            if *b == b'$' || *b == b'#' || *b == b'*' || *b == b'}' {
+                buffer.push(b'}');
+                buffer.push(*b ^ 0x20);
+                checksum = checksum.wrapping_add(b'}');
+                checksum = checksum.wrapping_add(*b ^ 0x20);
+            } else {
+                buffer.push(*b);
+                checksum = checksum.wrapping_add(*b);
+            }
+        }
+        buffer.push(b'#');
+        buffer.push(HEXCHARS[checksum as usize >> 4]);
+        buffer.push(HEXCHARS[checksum as usize & 0xf]);
+        socket.write_all(&buffer)?;
+        socket.flush()?;
+
+        Ok(())
+    }
+}
+
+impl<'a> GDBServer<'a> {
+    pub fn new<A: ToSocketAddrs>(uc: &unicorn::Unicorn, address: A) -> std::io::Result<GDBServer> {
+        Ok(GDBServer {
+            uc: uc,
+            listener: TcpListener::bind(address)?,
+        })
+    }
+
+    pub fn accept(&self) -> std::io::Result<GDBSession> {
+        let (socket, _addr) = self.listener.accept()?;
+        Ok(GDBSession::new(self.uc, socket))
     }
 }
